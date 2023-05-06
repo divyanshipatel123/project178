2f4d6185e46500a318b694630d500cc0574ee7d4
unknown <divyanshipatel2008apr13@gmail.com>
2023-05-06 10:03:46 +0530

    change in project 180

--- ar_weather.js
+++ ar_weather.js
@@ -1,6 +1,7 @@
 var coordinates = {}
 $(document).ready(function(){
     getCoordinates()
+    getWeather()
 })
 function getCoordinates(){
     let search_params = new URLSearchParams(window.location.search)
@@ -20,4 +21,31 @@ function getCoordinates(){
         alert("coordinates are not selected")
         window.history.back()
     }
+}
+function getWeather(){
+    $.ajax({
+        url:`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.destination_lat}&lon=${coordinates.destination_lon}&appid=f5ecb1f95d8e32f30c12be3eb3e98769`,
+        type:"get",
+        success:function(response){
+            let name = response.name;
+            let weather = response.weather[0].main
+            let temp = response.main.temp - 273
+            console.log("the weather: " , weather)
+            console.log("name" , name)
+            console.log(response , "    response")
+
+            temp = temp.toString().substring(0, 5)
+
+            console.log("temp: " , temp)
+            $('scene_container').append(
+                `
+                <a-entity gps-entity-place="latitude:${coordinates.destination_lat}; longitude:${coordinates.destination_lon}">
+                    <a-entity>
+                        <a-text height="50" value="The Weather Forecast is ${weather} at ${name} \n The temperature is ${temp}*C"></a-text>
+                    </a-entity>
+                </a-entity>
+                `
+            )
+        }
+    })
 }
